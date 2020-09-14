eaf0f503084e00fbbf01c0da5bd3dc71d9559821
Wildan Hanani <wildanhanani@gmail.com>
2020-09-14 23:59:08 +0700

    add: schema merchant

--- midlleware/schema.js
+++ midlleware/schema.js
@@ -14,3 +14,19 @@ exports.midRegister = (req, res, next) => {
   }
   next();
 };
+
+exports.midMercahnt = (req, res, next) => {
+  const schema = Joi.object({
+    user: Joi.string().required(),
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    name_bank: Joi.string().required(),
+    rekening: Joi.string().required(),
+  }).options({ abortEarly: false });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return validasi_data(res, 'Validation Error', error.details);
+  }
+  next();
+};
