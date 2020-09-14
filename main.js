acfc84467b4224ea0e87f79de65e8096bedef830
bagus priambudi <baguspriambudi77@gmail.com>
2020-09-14 20:34:28 +0700

    add git init in new repository

---
+++ main.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const PORT = 4000
+const dotenv = require('dotenv')
+
+dotenv.config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('combined'))
+
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("mongodb connected"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+const router = express.Router()
+app.use('/api/v1', router)
+
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 400;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        status: error.status || 500,
+        error: error.message,
+    })
+})
+
+app.listen(PORT, console.log('listening to PORT ' + PORT))
