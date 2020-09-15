302a97b522abf53131d04ce6047c184e0cea97e7
Wildan Hanani <wildanhanani@gmail.com>
2020-09-15 17:02:18 +0700

    add: update wallet in route, endpoint in main  and fix: wallet route

--- route/wallet_route.js
+++ route/wallet_route.js
@@ -13,16 +13,32 @@ exports.wallet = async (req, res, next) => {
   try {
     const user = req.user._id;
     const dana = req.body.dana;
-    console.log(dana);
     const finduser = await User.findOne({ _id: user });
     if (!finduser) {
       return data_notfound(res, 'user not found');
+    }
+    const findwallet = await Wallet.findOne({ user: user });
+    if (findwallet) {
+      return validasi(res, 'user has created wallet');
     }
     const wallet = await new Wallet({
       dana: dana,
       user: user,
     }).save();
     respone_ok_data(res, 'fund filled in the wallet', wallet);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updatewallet = async (req, res, next) => {
+  try {
+    const findwallet = await Wallet.findOne({ _id: req.query.id });
+    if (!findwallet) {
+      data_notfound(res, 'Id wallet not found');
+    }
+    const wallet = await Wallet.updateOne({ _id: findwallet }, { dana: +req.body.dana + +(+findwallet.dana) });
+    respone_ok_data(res, 'fund succesfull added', wallet);
   } catch (error) {
     next(error);
   }
